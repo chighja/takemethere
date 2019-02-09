7e27e7d89d0768a8f1a01226458486fc6a258080
chighja <chighja@gmail.com>
2019-02-09 11:13:44 -0500

    removed database environment variables from code

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 'use strict';
 
-exports.DATABASE_URL =
-  process.env.DATABASE_URL ||
-  'mongodb://admin:Password1@ds161008.mlab.com:61008/top-locations';
+exports.DATABASE_URL = process.env.DATABASE_URL;
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL ||
-  'mongodb://admin:Password1@ds161008.mlab.com:61008/top-locations';
+  'mongodb://admin:Password1@ds161008.mlab.com:55794/top-locations-test';
 exports.PORT = process.env.PORT || 5000;
