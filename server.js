6b5147cfc5cbea57791c9863368f45bb762a358a
chighja <chighja@gmail.com>
2019-02-27 16:47:21 -0500

    added socket.io package and code

--- server.js
+++ server.js
@@ -5,10 +5,28 @@ const mongoose = require('mongoose');
 const { DATABASE_URL, PORT } = require('./config');
 const { Place } = require('./models/place');
 const { UserComment } = require('./models/userComment');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 const app = express();
 app.use(express.json());
 app.use(express.static('public'));
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
 
 // connecting to mongoDb
 mongoose.connect(DATABASE_URL, { useNewUrlParser: true });
