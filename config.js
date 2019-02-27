8b35389ac105ec4a87f85737ab322e767522a1c2
chighja <chighja@gmail.com>
2019-02-27 16:19:31 -0500

    updated env vars

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL;
+exports.DATABASE_URL =
+  process.env.DATABASE_URL ||
+  'mongodb://useradmin:Password1@ds161008.mlab.com:61008/top-locations';
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL ||
   'mongodb://admin:Password1@ds161008.mlab.com:55794/top-locations-test';
